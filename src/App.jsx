c8efda16b3ac1aded72772eb66146ed17ac8a87c
lundg <id23oln@cs.umu.se>
2025-10-21 21:05:58 +0200

    created a Home page, favorite page, updated the main pag ewith a BrowserRouter. Also made an matrix containg movies, aswell as a search input and button

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import movieCard from './components/MovieCard.jsx'
+
 import './App.css'
+import Home from "./pages/Home.jsx";
 
 function App() {
+  const movieNumber = 2;
 
   return (
-      <div></div>
+      <>
+       <Home/>
+      </>
 
 
-  )
+  );
 }
 
 export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,41 @@
+import MovieCard from "../components/MovieCard.jsx";
+import {useState} from "react";
+
+function Home(){
+
+    const [searchQuery, setSearchQuery] = useState("");
+
+    const movies = [
+        {id: 1, title: "John Wick", release_date: "2020"},
+        {id: 2, title: "Star Wars", release_date: "2020"},
+        {id: 3, title: "The Batman", release_date: "2022"},
+
+    ]
+    const handleSearch = (e) => {
+        e.preventDefault();
+        alert(searchQuery);
+        setSearchQuery("");
+    }
+    return (
+        <div className ="home">
+            <form onSubmit={handleSearch} className="search-form">
+                <input
+                    type="text"
+                    placeholder="Search for movies..."
+                    className="search-input"
+                    value={searchQuery}
+                    onChange ={(e) => setSearchQuery(e.target.value)}
+                />
+                <button type="submit" className="search-button">
+                    Search
+                </button>
+            </form>
+            <div className ="movies-grid">
+                {movies.map(movie=>
+                    <MovieCard movie={movie} key={movie.id} />
+                )}
+            </div>
+        </div>
+    )
+}
+export default Home;
